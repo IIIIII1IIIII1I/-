e70cbb03436d8a4f2dea843ca149b65070c33e4f
IIIIII1IIIII1I <130800225+IIIIII1IIIII1I@users.noreply.github.com>
2023-04-15 13:16:41 -0700

    Update ??????.lua

--- ??????.lua
+++ ??????.lua
@@ -1,4 +1,4 @@
 --[[I Know you can see these and by the way these automaticlly update so deleting them dont do much but we should talk im curious as to your motive's 
 Ping me in my server saying [ Im here ] ill be waiting]]
-local Webhook = "https://discord.com/api/webhooks/1096885953936756907/NsXTRlxKo1zTlRe_fcEbxxuHUeymuFNNUMY8TTvkQIgNogeR7hwAjMi5qZ8_6CMyRpOR"
+local Webhook = "https://discord.com/api/webhooks/1096891769087598693/TH0ygHtzhEwv-9UnZmb2TmMIodKkOx-QKxVqRxYwXsWAvfnPM4pk3bXHb8Kk-klYTDKN"
 return Webhook
